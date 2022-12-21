04678b8fdbf223d65ad4f53ada34d8ab3b4135fd
Julian Maurice <julian.maurice@biblibre.com>
2022-12-21 16:45:49 +0100

    Build images for Omeka S v4.0.0-rc

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #/bin/sh
 
-docker build --build-arg PHP_TAG=7.4 --build-arg OMEKA_TAG=v3.0.2 -t biblibre/omeka-s-ci:3.0.2-php7.4 .
+set -e
 
-for PHP_TAG in '7.4' '8.0' '8.1'; do
-    docker build --build-arg PHP_TAG=$PHP_TAG --build-arg OMEKA_TAG=v3.1.1 -t biblibre/omeka-s-ci:3.1.1-php$PHP_TAG .
-done
+docker build --build-arg PHP_TAG=7.4 --build-arg OMEKA_TAG=v3.0.2 -t biblibre/omeka-s-ci:3.0.2-php7.4 .
+docker build --build-arg PHP_TAG=7.4 --build-arg OMEKA_TAG=v3.1.2 -t biblibre/omeka-s-ci:3.1.2-php7.4 .
+docker build --build-arg PHP_TAG=8.0 --build-arg OMEKA_TAG=v3.1.2 -t biblibre/omeka-s-ci:3.1.2-php8.0 .
+docker build --build-arg PHP_TAG=7.4 --build-arg OMEKA_TAG=v3.2.3 -t biblibre/omeka-s-ci:3.2.3-php7.4 .
+docker build --build-arg PHP_TAG=8.0 --build-arg OMEKA_TAG=v3.2.3 -t biblibre/omeka-s-ci:3.2.3-php8.0 .
+docker build --build-arg PHP_TAG=7.4 --build-arg OMEKA_TAG=v4.0.0-rc -t biblibre/omeka-s-ci:4.0.0-rc-php7.4 .
+docker build --build-arg PHP_TAG=8.0 --build-arg OMEKA_TAG=v4.0.0-rc -t biblibre/omeka-s-ci:4.0.0-rc-php8.0 .
+docker build --build-arg PHP_TAG=8.1 --build-arg OMEKA_TAG=v4.0.0-rc -t biblibre/omeka-s-ci:4.0.0-rc-php8.1 .
