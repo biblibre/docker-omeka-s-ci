98f7781fc5334135ebd02f71c56f4edf2fcd13c3
Julian Maurice <julian.maurice@biblibre.com>
2023-01-20 10:21:22 +0100

    One Dockerfile per version (add Dockerfiles for 4.0.0 and PHP 8.2)

--- build.sh
+++ build.sh
@@ -2,11 +2,14 @@
 
 set -e
 
-docker build --build-arg PHP_TAG=7.4 --build-arg OMEKA_TAG=v3.0.2 -t biblibre/omeka-s-ci:3.0.2-php7.4 .
-docker build --build-arg PHP_TAG=7.4 --build-arg OMEKA_TAG=v3.1.2 -t biblibre/omeka-s-ci:3.1.2-php7.4 .
-docker build --build-arg PHP_TAG=8.0 --build-arg OMEKA_TAG=v3.1.2 -t biblibre/omeka-s-ci:3.1.2-php8.0 .
-docker build --build-arg PHP_TAG=7.4 --build-arg OMEKA_TAG=v3.2.3 -t biblibre/omeka-s-ci:3.2.3-php7.4 .
-docker build --build-arg PHP_TAG=8.0 --build-arg OMEKA_TAG=v3.2.3 -t biblibre/omeka-s-ci:3.2.3-php8.0 .
-docker build --build-arg PHP_TAG=7.4 --build-arg OMEKA_TAG=v4.0.0-rc -t biblibre/omeka-s-ci:4.0.0-rc-php7.4 .
-docker build --build-arg PHP_TAG=8.0 --build-arg OMEKA_TAG=v4.0.0-rc -t biblibre/omeka-s-ci:4.0.0-rc-php8.0 .
-docker build --build-arg PHP_TAG=8.1 --build-arg OMEKA_TAG=v4.0.0-rc -t biblibre/omeka-s-ci:4.0.0-rc-php8.1 .
+tags=""
+tags="$tags 3.0.2-php7.4"
+tags="$tags 3.1.2-php8.0"
+tags="$tags 3.2.3-php8.0"
+tags="$tags 4.0.0-php8.0"
+tags="$tags 4.0.0-php8.1"
+tags="$tags 4.0.0-php8.2"
+
+for tag in $tags; do
+    docker build -t biblibre/omeka-s-ci:$tag $tag
+done
