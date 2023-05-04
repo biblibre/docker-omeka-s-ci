58bdf1556675139e538f24e3a47f3ed1cae38bf2
Julian Maurice <julian.maurice@biblibre.com>
2023-05-04 09:40:34 +0200

    Add 3.1.2-php7.4 and 3.2.3-php7.4

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ set -e
 
 tags=""
 tags="$tags 3.0.2-php7.4"
+tags="$tags 3.1.2-php7.4"
 tags="$tags 3.1.2-php8.0"
+tags="$tags 3.2.3-php7.4"
 tags="$tags 3.2.3-php8.0"
 tags="$tags 4.0.0-php8.0"
 tags="$tags 4.0.0-php8.1"
@@ -14,5 +16,5 @@ tags="$tags 4.0.1-php8.1"
 tags="$tags 4.0.1-php8.2"
 
 for tag in $tags; do
-    docker build -t biblibre/omeka-s-ci:$tag $tag
+    docker build -t ghcr.io/biblibre/omeka-s-ci:$tag $tag
 done
